a171a4d64b41f22f4445e92b63c37690da7784cc
Clever Coder <clevercoderr@gmail.com>
2021-12-22 11:47:01 +0500

    Add project files.

---
+++ Apose OCR Image To Text Whith to Csharp/AsposeOcr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apose_OCR_Image_To_Text_Whith_to_Csharp
+{
+    internal class AsposeOcr
+    {
+        internal string RecognizeLine(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Apose OCR Image To Text Whith to Csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Apose_OCR_Image_To_Text_Whith_to_Csharp
+{
+    internal class Program
+    {
+        public static object RunExamples { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            // For complete examples and data files, please go to https://github.com/aspose-ocr/Aspose.OCR-for-.NET
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_OCR();
+
+            // Initialize an instance of AsposeOcr
+            AsposeOcr api = new AsposeOcr();
+
+            // Recognize image
+            string result = api.RecognizeLine(dataDir + "sample_line.png");
+
+            // Display the recognized text
+            Console.WriteLine(result);
+
+        }
+    }
+}
